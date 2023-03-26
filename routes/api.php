11126f60d674a83734f3c54239b4d63388756e72
NguyenNam27 <nguyenngocnam00770@gmail.com>
2023-03-27 01:12:40 +0700

    update

---
+++ routes/api.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\API\PostsController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);
+    Route::post('/change-pass', [AuthController::class, 'changePassWord']);
+});
+
+
+//Route::post('login', [UserController::class, 'login']);
+//Route::post('register', [UserController::class, 'register']);
+//Route::post('logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
+
+//Route::group(['prefix' => 'auth','middleware' => 'api'])->group(function()  {
+//    Route::get('/', [PostsController::class,'index']);
+//    Route::post('add', [PostsController::class,'add']);
+//    Route::post('update/{id}', [PostsController::class,'update']);
+//    Route::get('edit/{id}', [PostsController::class,'edit']);
+//    Route::delete('delete/{id}', [PostsController::class,'delete']);
+//});
